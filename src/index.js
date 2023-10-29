5ef402e745b507513eae8d38fc12155f6db73d66
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2023-10-29 13:10:36 -0300

    config att

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const session = require('express-session');
 const root = require('./root');
 const app = express();
 const { join } = require('path');
+require('dotenv').config()
 
+app.use(session({ secret: process.env.private_key, resave: true, saveUninitialized: true }));
 app.use(express.static(__dirname + '/public'));
 
 app.use('/static', express.static(join(process.cwd(),"views")))
