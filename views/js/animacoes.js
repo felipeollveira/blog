4d81ef2cc9b5789b875eeea1c4886fc17ec1da08
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2023-11-17 14:49:24 -0300

    new design of the menu and animation

--- views/js/animacoes.js
+++ views/js/animacoes.js
@@ -1,14 +1,17 @@
 const dropdown = document.getElementById('dropdown');
+const autor = document.getElementById('autor');
 const menu = document.getElementById('menu');
 const grids = document.getElementById('grid');
 
-dropdown.onclick = function() {
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none'
-    }else{
 
-        menu.style.display = 'block'
+
+dropdown.onclick = function() {
+        if (menu.style.display === 'block') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+        }
     }
 
-    grids.classList.toggle('home-grid-mobile');
-};
+
+
