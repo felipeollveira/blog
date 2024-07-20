11b1d12b9703dfbcc778ca7408fcdc308a098595
OLIVEIRA <foliv132@ford.com>
2024-07-20 13:00:41 -0300

    Depois de muito tempo, olá novamente, blog.

--- views/js/aside.js
+++ views/js/aside.js
@@ -3,7 +3,6 @@ const content = document.getElementById('menu')
 const arrow = document.getElementById('arrow')
 
 dropdown.addEventListener('click', () => {
-    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
     arrow.style.transform = (content.style.display === 'block') ? 'rotate(180deg)' : 'rotate(360deg)';
 });
 
