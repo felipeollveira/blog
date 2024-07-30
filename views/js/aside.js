d7914ff987881f75ac69d726fd0c169270382ccf
OLIVEIRA <FOLIV132@ford.com>
2024-07-29 22:26:14 -0300

    new features to authors profiles and resposivity css

--- views/js/aside.js
+++ views/js/aside.js
@@ -1,9 +1,21 @@
 const dropdown = document.getElementById('dropdown')
 const content = document.getElementById('menu')
 const arrow = document.getElementById('arrow')
+const autor = document.querySelectorAll(`.autor-info`)
+const dropdownprofiles = document.getElementById('dropdown-acc')
 
 dropdown.addEventListener('click', () => {
-    arrow.style.transform = (content.style.display === 'block') ? 'rotate(180deg)' : 'rotate(360deg)';
+    if(dropdownprofiles.style.display === 'none'){
+        dropdownprofiles.style.display ='block'
+        arrow.style.display ='none'
+        autor.style.display = 'none'
+    }else{
+        dropdownprofiles.style.display = 'none'
+    }
 });
 
+const profilesModal = () => {
+    dropdownprofiles.style.display = 'none'
+    arrow.style.display ='inline-block'
 
+}
