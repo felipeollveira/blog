595a11b55169dc18cfb741437f044fabce4a3e51
OLIVEIRA <FOLIV132@ford.com>
2024-07-31 18:14:33 -0300

    new animat css

--- views/js/aside.js
+++ views/js/aside.js
@@ -1,21 +1,27 @@
 const dropdown = document.getElementById('dropdown')
 const content = document.getElementById('menu')
 const arrow = document.getElementById('arrow')
-const autor = document.querySelectorAll(`.autor-info`)
+const autor = document.getElementById('autor')
 const dropdownprofiles = document.getElementById('dropdown-acc')
+const asideActions = document.querySelector('.aside-actions');
 
 dropdown.addEventListener('click', () => {
-    if(dropdownprofiles.style.display === 'none'){
+    if(dropdownprofiles){
         dropdownprofiles.style.display ='block'
         arrow.style.display ='none'
-        autor.style.display = 'none'
+
+        asideActions.classList.add('visibility-05');
     }else{
         dropdownprofiles.style.display = 'none'
+
+
     }
 });
 
 const profilesModal = () => {
     dropdownprofiles.style.display = 'none'
     arrow.style.display ='inline-block'
+    asideActions.classList.remove('visibility-05');
+
 
 }
